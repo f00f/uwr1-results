dfef6f93baa29769dc07040e21039a66f85a154a
f00f <582746+f00f@users.noreply.github.com>
2010-09-29 00:42:31 +0000

    Filled -v2 API files

--- views/uwr1results-ajax-ranking-v2.php
+++ views/uwr1results-ajax-ranking-v2.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: uwr1results Ajax Ranking View
+Template Name: uwr1results Ajax Ranking View v2
 Plugin URI: http://uwr1.de/
 Author: Hannes Hofmann
 Author URI: http://uwr1.de/
@@ -17,20 +17,38 @@ $ranking =& $league->ranking();
 $rankingFlat = array();
 $rank = 0;
 foreach ($ranking as $r) {
+	if (!is_int($r['goalsDiff'])) { $r['goalsDiff'] = '"' . $r['goalsDiff'] . '"'; }
+	if (!is_int($r['pointsPos'])) { $r['pointsPos'] = '"' . $r['pointsPos'] . '"'; }
 	$rankingFlat[] = '{'
-		. '"rank":'       . sprintf('%02d', ++$rank)
-		. ',"team":'      . '"' . htmlentities($r['name'], ENT_COMPAT, 'UTF-8') . '"'
-		. ',"tore":'      . '"' . $r['goalsPos'] . ':' . $r['goalsNeg'] . '"'
-		. ',"tordiff":'   . $r['goalsDiff']
-		. ',"punkte":'    . $r['pointsPos']
-		. ',"spiele":'    . $r['matchesPlayed']
+		// r = rang
+		. '"r":'       . ++$rank // '"' . sprintf('%02d', ++$rank) . '"'
+		// m = mannschaft
+		. ',"m":'      . '"' . htmlentities($r['name'], ENT_COMPAT, 'UTF-8') . '"'
+		// t = tore
+		. ',"t":'      . '"' . $r['goalsPos'] . ':' . $r['goalsNeg'] . '"'
+		// d = (tor)differenz
+		. ',"d":'   . $r['goalsDiff']
+		// p = punkte
+		. ',"p":'    . $r['pointsPos']
+		// s = spiele (gespielt)
+		. ',"s":'    . $r['matchesPlayed']
 		. '}';
 }
 
-print @$_GET['jsonp'].'({"uwr1results":'
-	. '['; // open ranking array
-print implode(',', $rankingFlat);
-print ']' // close uwr1results array
+$count = count($ranking);
+if ($count > 0) {
+	$status = 0;
+} else {
+	$status = -1;
+}
+
+print @$_GET['jsonp'].'({'
+	. '"s":'.$status
+//	. ',"type":"rnk"'
+	. ',"cnt":' . $count
+	. ',"res":[' // open results array
+	. implode(',', $rankingFlat)
+	. ']' // close results array
 	.'})'; // close anon object and )
 
 exit;
