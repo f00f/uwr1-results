eb60880ec84c04cf032e7da050137335f53d158e
f00f <582746+f00f@users.noreply.github.com>
2010-10-01 06:38:27 +0000

    Bumped version to 1.0.1

--- uwr1res_config.inc.php
+++ uwr1res_config.inc.php
@@ -37,7 +37,7 @@ define('UWR1RESULTS_JSON_CACHE_UPDATE_URL', 'http://uwr1cdn.appspot.com/jc/updat
 
 # END OF CONFIG
 
-define('UWR1RESULTS_VERSION', '0.7.2');
+define('UWR1RESULTS_VERSION', '1.0.1');
 
 define('UWR1RESULTS_TBL_REGIONS',   UWR1RESULTS_TBL_PRE . 'regions'  );
 define('UWR1RESULTS_TBL_LEAGUES',   UWR1RESULTS_TBL_PRE . 'leagues'  );
