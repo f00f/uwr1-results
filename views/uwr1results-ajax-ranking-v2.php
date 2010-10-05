593dae894a61b71c472349f6a53185377ee304cb
f00f <582746+f00f@users.noreply.github.com>
2010-10-05 14:00:12 +0000

    Added correct Content-type for JSON response

--- views/uwr1results-ajax-ranking-v2.php
+++ views/uwr1results-ajax-ranking-v2.php
@@ -6,6 +6,8 @@ Author: Hannes Hofmann
 Author URI: http://uwr1.de/
 */
 
+header('Content-type: application/json');
+
 // this should be in the controller, but it's more convenient to have it here - for now.
 $league =& Uwr1resultsModelLeague::instance();
 $ranking =& $league->ranking();
