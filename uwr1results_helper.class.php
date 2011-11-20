1e2af8ef56ac10e35710ce9a333ea935b0be3f59
f00f <582746+f00f@users.noreply.github.com>
2011-11-20 01:42:22 +0000

    capabilities moved into config

--- uwr1results_helper.class.php
+++ uwr1results_helper.class.php
@@ -37,7 +37,7 @@ class Uwr1resultsHelper {
 	public static function checkPermission( $perm='save' ) {
 		switch ( $perm ) {
 			case 'save':
-				return current_user_can('edit_posts');
+				return current_user_can(UWR1RESULTS_CAPABILITIES);
 				break;
 		}
 	}
